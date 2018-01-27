ef15b6fb6943b975161dcc2991caca86f9f4a50b
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-01-27 18:58:21 -0500

    Trying agian by calculating offset first, making an int pointer, and then dereferencing.

--- project1/library.c
+++ project1/library.c
@@ -131,7 +131,9 @@ void draw_pixel(int x, int y, color_t color)
     // To get correct col, add the input x
     // Tultiply together to get correct address,
     // then dereference and place the color bits at that address.
-    *(framebuffer + ((y * virtualResolution.xres_virtual) + x)) = color;
+    int offset = ((y * virtualResolution.xres_virtual) + x);
+    int* addr = framebuffer + offset;
+    *addr = color;
     
 }
 
