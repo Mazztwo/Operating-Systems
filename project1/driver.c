c7add753933553137366932fe7efcdaf0ee242bc
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-01-27 19:31:56 -0500

    Adding a pixel test

--- project1/driver.c
+++ project1/driver.c
@@ -6,11 +6,19 @@
 
 int main()
 {
-    //init_graphics();
+    init_graphics();
+    clear_screen();
     
-    //clear_screen();
+    color_t color;
+    color = encode_color(30, 0, 0);
     
-    //exit_graphics();
+    draw_pixel(50, 50, color);
+    
+    while(get_key() != 'q')
+    {
+        
+    }
+    exit_graphics();
 
     return 0;
 }
