535b23e983cbe5368cd6e09deacbefdde0e2e7c5
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-02 14:42:56 -0500

    Testing move circe up with w.

--- project1/driver.c
+++ project1/driver.c
@@ -8,34 +8,31 @@ int main()
 {
     char key = 'm';
     int x, y, width, height, radius;
+    int MAX_X = 639;
+    int MAX_Y = 479;
     
     init_graphics();
-    
     clear_screen();
-    
     
     color_t color;
     color = encode_color(0, 60, 0);
     
-   /*
-    draw_pixel(50, 50, color);
-    draw_pixel(50, 60, color);
-    draw_pixel(50, 70, color);
-    draw_pixel(50, 80, color);
-    draw_pixel(50, 90, color);
-    */
-
-
     x = 50, y = 50, width = 100, height = 100;
     draw_rect(x, y, width, height, color);
-    
     
     x = 200, y = 200, radius = 30;
     draw_circle(x, y, radius, color);
     
-    
     while(key != 'q')
     {
+        if(key == 'w')
+        {
+            if( !(y-radius < 0) )
+            {
+                draw_circle(x, y, radius, color);
+            }
+        }
+        
         
     	key = get_key();
     }
