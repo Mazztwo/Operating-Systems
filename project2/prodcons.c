14e335bc9170194aa0762d5bcbdcd31986c0ff5f
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-23 15:30:50 -0500

    Added shortcuts to up/down and a main function.

--- project2/prodcons.c
+++ project2/prodcons.c
@@ -5,9 +5,9 @@
 //
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
+#include<stdio.h>
+#include<sys/mman.h>
+#include<stdlib.h>
 
 
 // Semaphore struct
@@ -17,5 +17,29 @@ struct cs1550_sem
     struct node* head;
     struct node* tail;
 };
+
+
+// To make calling our syscalls more natural in C
+void down(cs1550_sem *sem)
+{
+    syscall(__NR_cs1550_down, sem);
+}
+
+void up(cs1550_sem *sem)
+{
+    syscall(__NR_cs1550_up, sem);
+}
+
+
+int main(int argc, char* argv[])
+{
+
+    
+    
+    
+    return 0;
+}
+
+
 
 
