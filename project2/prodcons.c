82108c822fc76dc42fe4c9d3fb48ae598fbb7362
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-23 22:11:54 -0500

    Initialized mmap()'ed buffer and sempahore space, and initialized semaphores.

--- project2/prodcons.c
+++ project2/prodcons.c
@@ -45,8 +45,6 @@ int main(int argc, char* argv[])
         down = 325
         up = 326
         syscall(325/326, sem);
-     
-     
     */
     
     
@@ -58,6 +56,38 @@ int main(int argc, char* argv[])
     // Make shared buffer before fork() so memory can be shared
     void *buffer = mmap(NULL, bufferSize, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
     
+    // Now initialize empty, full, and mutex
+    // I'm not really sure about whether or not the "buffer size" specified in the command line
+    // is a hard limit, and we can mmap a buffersize + sizeof(struct cs1550_sem)*3, or if we have to
+    // make two separate buffers. I'll go with making two separate buffers just in case the directions
+    // mean that our shared buffer is exactly the size specified in the command line, no more, no less.
+    void *semaphore_space = mmap(NULL, sizeof(struct cs1550_sem)*3, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
+    
+    struct cs1550_sem* empty;
+    struct cs1550_sem* full;
+    struct cs1550_sem* mutex;
+    
+    /* Map of semaphore_space:
+    
+     |__empty__|__full__|__mutex__|
+     |    0    |   1    |    2    |
+     
+    */
+    empty = semaphore_space;
+    full = semaphore_space + 1;
+    mutex = semaphore_space + 2;
+    
+    // Initial number of empty spaces is the whole buffer
+    empty->value = bufferSize;
+    
+    // There are no spots filled initially
+    full->value = 0;
+    
+    // Initialize mutex to 1 (unlocked)
+    mutex->value = 1;
+    
+  
+
     
     
     return 0;
