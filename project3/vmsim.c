afdfef07d6fce5a82aefc06debe08717e9b5e17e
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-03-28 19:27:11 -0400

    Project 3 initial commit.

---
+++ project3/vmsim.c
@@ -0,0 +1,22 @@
+//
+//  CS 1550 Operating Systems
+//  Alessio Mazzone
+//  Project3
+//
+
+
+#include<stdio.h>
+#include<sys/mman.h>
+#include<stdlib.h>
+
+
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
+
+
+
+
