2ec427b4720a4a0a6564649af478d6c0fbc76778
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-03-28 19:56:07 -0400

    Added OPT function prototype and some file i/o code to open trace files.

---
+++ Project33/Project33/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project33
+//
+//  Created by Alessio Mazzone on 3/28/18.
+//  Copyright © 2018 Alessio Mazzone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- project3/vmsim.c
+++ project3/vmsim.c
@@ -10,10 +10,30 @@
 #include<stdlib.h>
 
 
+// Tracefile
+FILE *traceFile;
+
+void OPT(int rawr)
+{
+    
+}
+
+
 
 int main(int argc, char* argv[])
 {
+    traceFile = fopen("gcc.trace", "r");
     
+    if(!traceFile)
+    {
+        printf("ERROR: Trace file could not be opened. Please try again.\n");
+        return 0;
+    }
+    
+    
+    printf("hallow\n");
+    
+    fclose(traceFile);
     return 0;
 }
 
